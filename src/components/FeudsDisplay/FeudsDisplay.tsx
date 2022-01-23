1581adf4b9bdbbad0aa889738bac77f8f9632661
vincentjr <contact@vincentjr.com>
2022-01-22 21:39:29 -0500

    fixing responsive style for awards

--- src/components/FeudsDisplay/FeudsDisplay.tsx
+++ src/components/FeudsDisplay/FeudsDisplay.tsx
@@ -1,53 +1,48 @@
 import React from "react";
 import { IFeud } from "../../api/types";
 import {
+  Box,
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
   TableCaption,
-  Text
-} from '@chakra-ui/react'
-import { MdHelp } from "react-icons/md";
+  Text,
+} from "@chakra-ui/react";
 
 export const FeudsDisplay: React.FC<{ feuds: IFeud[] }> = ({ feuds }) => {
   return (
-    <>
+    <Box w="100%" overflowX={"auto"}>
       <Text mt={5} fontSize="2xl">
         Top Feuds
       </Text>
-      <Table variant='striped' size="sm" width="400px">
-        <TableCaption>The most numerous head-to-head encounters of the match</TableCaption>
+      <Table variant="striped" size="sm" width="400px">
+        <TableCaption>
+          The most numerous head-to-head encounters of the match
+        </TableCaption>
         <Thead>
           <Tr>
             <Th>PlayerA</Th>
-            <Th >Kills</Th>
+            <Th>Kills</Th>
             <Th isNumeric>Kills</Th>
-            <Th >PlayerB</Th>
+            <Th>PlayerB</Th>
           </Tr>
         </Thead>
         <Tbody>
           {feuds.map((feud, idx) => (
-              <Tr key={idx} px={2} backgroundColor="rgba(0, 0, 0, 0.15)" w="100%">
-                  <Td>
-                    {feud[0]}
-                  </Td>
-                  <Td fontWeight="bold" >
-                    {feud[2]}
-                  </Td>
-                  <Td isNumeric fontWeight="bold" >
-                    {feud[3]}
-                  </Td>
-                  <Td>
-                    {feud[1]}
-                  </Td>
-              </Tr>
+            <Tr key={idx} px={2} backgroundColor="rgba(0, 0, 0, 0.15)" w="100%">
+              <Td>{feud[0]}</Td>
+              <Td fontWeight="bold">{feud[2]}</Td>
+              <Td isNumeric fontWeight="bold">
+                {feud[3]}
+              </Td>
+              <Td>{feud[1]}</Td>
+            </Tr>
           ))}
         </Tbody>
       </Table>
-    </>
+    </Box>
   );
 };
